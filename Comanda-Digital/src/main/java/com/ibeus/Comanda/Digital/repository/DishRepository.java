613c92a54daccbd1f6ddf22510ce138e69c6f723
Felipe Teodolino <felipe.gt1355@gmail.com>
2025-12-01 11:41:18 -0300

    feat: implementação do DishRepository

--- Comanda-Digital/src/main/java/com/ibeus/Comanda/Digital/repository/DishRepository.java
+++ Comanda-Digital/src/main/java/com/ibeus/Comanda/Digital/repository/DishRepository.java
@@ -2,6 +2,7 @@ package com.ibeus.Comanda.Digital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ibeus.Comanda.Digital.model.Dish;
+// Comentário de atualização para commit
 
 public interface DishRepository extends JpaRepository<Dish, Long> {
 }
